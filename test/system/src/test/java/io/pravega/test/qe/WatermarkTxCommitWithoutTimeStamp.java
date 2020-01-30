b64fe8b6b1e512a398245ce67d1aa7d50a6e38a5
Raghuveera Rao <raghuveera.rao@emc.com>
2020-01-30 23:16:21 +0530

    watermark tx and automatic notetime tests added

---
+++ test/system/src/test/java/io/pravega/test/qe/WatermarkTxCommitWithoutTimeStamp.java
@@ -0,0 +1,186 @@
+/**
+ * Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.pravega.test.qe;
+
+import io.pravega.client.ClientConfig;
+import io.pravega.client.EventStreamClientFactory;
+import io.pravega.client.SynchronizerClientFactory;
+import io.pravega.client.admin.StreamManager;
+import io.pravega.client.netty.impl.ConnectionFactory;
+import io.pravega.client.netty.impl.ConnectionFactoryImpl;
+import io.pravega.client.state.Revision;
+import io.pravega.client.state.RevisionedStreamClient;
+import io.pravega.client.state.SynchronizerConfig;
+import io.pravega.client.stream.*;
+import io.pravega.client.stream.impl.ControllerImpl;
+import io.pravega.client.stream.impl.ControllerImplConfig;
+import io.pravega.client.stream.impl.JavaSerializer;
+import io.pravega.client.watermark.WatermarkSerializer;
+import io.pravega.common.Exceptions;
+import io.pravega.common.concurrent.Futures;
+import io.pravega.common.hash.RandomFactory;
+import io.pravega.controller.store.stream.StoreException;
+import io.pravega.shared.NameUtils;
+import io.pravega.shared.watermarks.Watermark;
+import io.pravega.test.common.AssertExtensions;
+import io.pravega.test.system.framework.Environment;
+import io.pravega.test.system.framework.SystemTestRunner;
+import io.pravega.test.system.framework.Utils;
+import io.pravega.test.system.framework.services.Service;
+import lombok.Cleanup;
+import lombok.extern.slf4j.Slf4j;
+import mesosphere.marathon.client.MarathonException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+import org.junit.runner.RunWith;
+
+import java.net.URI;
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertTrue;
+
+@Slf4j
+@RunWith(SystemTestRunner.class)
+public class WatermarkTxCommitWithoutTimeStamp extends AbstractSystemTest {
+
+    private static final String STREAM = "testWMTxCommitStream";
+    private static final String SCOPE = "testWMTxCommitScope" + RandomFactory.create().nextInt(Integer.MAX_VALUE);
+
+    @Rule
+    public Timeout globalTimeout = Timeout.seconds(10 * 60);
+
+    private final ScalingPolicy scalingPolicy = ScalingPolicy.fixed(5);
+    private final StreamConfiguration config = StreamConfiguration.builder().scalingPolicy(scalingPolicy).build();
+    private Service controllerInstance;
+    private URI controllerURI;
+    private StreamManager streamManager;
+    private final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(5);
+
+    /**
+     * This is used to setup the various services required by the system test framework.
+     *
+     * @throws MarathonException    when error in setup
+     */
+    @Environment
+    public static void initialize() throws MarathonException, ExecutionException {
+        URI zkUri = startZookeeperInstance();
+        startBookkeeperInstances(zkUri);
+        URI controllerUri = startPravegaControllerInstances(zkUri, 2);
+        ensureSegmentStoreRunning(zkUri, controllerUri);
+    }
+
+    @Before
+    public void setup() {
+        controllerInstance = Utils.createPravegaControllerService(null);
+        List<URI> ctlURIs = controllerInstance.getServiceDetails();
+        final List<String> uris = ctlURIs.stream().filter(ISGRPC).map(URI::getAuthority).collect(Collectors.toList());
+
+        controllerURI = URI.create("tcp://" + String.join(",", uris));
+        streamManager = StreamManager.create(Utils.buildClientConfig(controllerURI));
+        assertTrue("Creating Scope", streamManager.createScope(SCOPE));
+        assertTrue("Creating stream", streamManager.createStream(SCOPE, STREAM, config));
+    }
+
+    @After
+    public void tearDown() {
+        streamManager.close();
+    }
+
+    @Test
+    public void verifyWaterMarkTxWithoutTimestamp() throws Exception {
+        final ClientConfig clientConfig = Utils.buildClientConfig(controllerURI);
+        @Cleanup
+        ConnectionFactory connectionFactory = new ConnectionFactoryImpl(clientConfig);
+        ControllerImpl controller = new ControllerImpl(ControllerImplConfig.builder().clientConfig(clientConfig).build(),
+                connectionFactory.getInternalExecutor());
+
+        // create one writer
+        @Cleanup
+        EventStreamClientFactory clientFactory = EventStreamClientFactory.withScope(SCOPE, clientConfig);
+        JavaSerializer<Long> javaSerializer = new JavaSerializer<>();
+        @Cleanup
+        TransactionalEventStreamWriter<Long> writer1 = clientFactory.createTransactionalEventWriter(STREAM, javaSerializer,
+                EventWriterConfig.builder().build());
+
+        AtomicBoolean stopFlag = new AtomicBoolean(false);
+        // write transactional events
+        writeTxEvents(writer1, stopFlag);
+
+        // wait until mark stream is created
+        AtomicBoolean markStreamCreated = new AtomicBoolean(false);
+        Futures.loop(() -> !markStreamCreated.get(),
+                () -> Futures.exceptionallyExpecting(controller.getCurrentSegments(SCOPE, NameUtils.getMarkStreamForStream(STREAM)),
+                        e -> Exceptions.unwrap(e) instanceof StoreException.DataNotFoundException, null)
+                        .thenAccept(v -> markStreamCreated.set(v != null)), executorService);
+
+        @Cleanup
+        SynchronizerClientFactory syncClientFactory = SynchronizerClientFactory.withScope(SCOPE, clientConfig);
+        String markStream = NameUtils.getMarkStreamForStream(STREAM);
+
+        RevisionedStreamClient<Watermark> watermarkReader = syncClientFactory.createRevisionedStreamClient(markStream,
+                new WatermarkSerializer(),
+                SynchronizerConfig.builder().build());
+
+        LinkedBlockingQueue<Watermark> watermarks = new LinkedBlockingQueue<>();
+        fetchWatermarks(watermarkReader, watermarks, stopFlag);
+
+        AssertExtensions.assertEventuallyEquals(true, () -> watermarks.size() == 0, 100000);
+    }
+
+    private void fetchWatermarks(RevisionedStreamClient<Watermark> watermarkReader, LinkedBlockingQueue<Watermark> watermarks, AtomicBoolean stop) throws Exception {
+        AtomicReference<Revision> revision = new AtomicReference<>(watermarkReader.fetchOldestRevision());
+
+        Futures.loop(() -> !stop.get(), () -> Futures.delayedTask(() -> {
+            Iterator<Map.Entry<Revision, Watermark>> marks = watermarkReader.readFrom(revision.get());
+            if (marks.hasNext()) {
+                Map.Entry<Revision, Watermark> next = marks.next();
+                log.info("watermark = {}", next.getValue());
+                watermarks.add(next.getValue());
+                revision.set(next.getKey());
+            }
+            return null;
+        }, Duration.ofSeconds(10), executorService), executorService);
+    }
+
+    private CompletableFuture<Void> writeTxEvents(TransactionalEventStreamWriter<Long> writer, AtomicBoolean stopFlag) {
+        AtomicInteger count = new AtomicInteger(0);
+        AtomicLong timer = new AtomicLong();
+        AtomicLong currentTime = new AtomicLong();
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+        return Futures.loop(() -> !stopFlag.get(), () -> Futures.delayedFuture(() -> {
+            Transaction<Long> txn = writer.beginTxn();
+            return CompletableFuture.runAsync(() -> {
+                try {
+                    for (int i = 0; i < 10; i++) {
+                        currentTime.set(timer.incrementAndGet());
+                        txn.writeEvent(count.toString(), currentTime.get());
+                    }
+                    log.info("Note Time: ", currentTime.get());
+                    txn.commit();
+                } catch (TxnFailedException e) {
+                    throw new CompletionException(e);
+                }
+            });
+        }, 1000L, executorService), executorService);
+    }
+
+}
